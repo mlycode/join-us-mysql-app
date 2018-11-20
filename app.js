004b6aabb792d292d8c73b1a1650d8e5faee5d8b
mlycode <m.ly@live.co.uk>
2018-11-20 15:00:02 +0000

    add form and route to post / insert into db new user

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 require("dotenv").config();
 
 const express = require("express");
+const app = express();
 const faker = require("faker");
-
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
-const app = express();
+app.set("view engine", "ejs");
+app.use (bodyParser.urlencoded({extended: true}));
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: process.env.DB_PASS,
     database: 'join_us'
-});
-
-connection.connect((error) => {
-    if (error) {
-        throw error;
-    }
-    console.log("MySQL Connected");
 });
 
 //Inserting 500 random users
@@ -34,7 +30,27 @@ connection.connect((error) => {
 //     console.log(results);
 // });
 
-connection.end();
+//ROUTES
+app.get("/", (req, res) => {
+    const countUsers = "SELECT COUNT(*) AS total FROM users";
+    connection.query(countUsers, (err, results) => {
+        if (err) throw err;
+        const totalUsers = results[0].total;
+        // res.send("We have " + totalUsers + " users in our db");
+        res.render("home", {totalUsers: totalUsers});
+    });
+});
+
+app.post("/register", (req, res) => {
+    const newUser = {
+        email: req.body.email
+    };
+    const insertUser = "INSERT INTO users SET ?";
+    connection.query(insertUser, newUser, (err, results) => {
+        if (err) throw err;
+        res.redirect("/");
+    });
+});
 
 app.listen("3000", () => {
     console.log("server has started on port 3000");
--- db-schema.sql
+++ db-schema.sql
@@ -2,8 +2,3 @@ CREATE TABLE users (
     email VARCHAR(255) PRIMARY KEY,
     created_at TIMESTAMP DEFAULT NOW()
 );
-
---INSERT TEST USERS
-INSERT INTO users (email) VALUES
-("abc123@gmail.com"),
-("TT@outlook.co.uk");
